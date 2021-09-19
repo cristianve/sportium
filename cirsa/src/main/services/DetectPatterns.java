d05582c85f1425cbe470991cbdf3b2680506b109
Cristian Vega <97cvega@gmail.com>
2021-09-20 00:52:28 +0200

    comment all System.out.println()

--- cirsa/src/main/services/DetectPatterns.java
+++ cirsa/src/main/services/DetectPatterns.java
@@ -57,7 +57,7 @@ public class DetectPatterns {
 
             firstPart += " " + list.get(i);
         }
-        System.out.println("First Part: " + firstPart);
+        //System.out.println("First Part: " + firstPart);
 
         //Second part Split
         String secondPart = "";
@@ -65,22 +65,22 @@ public class DetectPatterns {
 
             secondPart += " " + list.get(i);
         }
-        System.out.println("Second Part: " + secondPart);
+        //System.out.println("Second Part: " + secondPart);
 
         //Name A
         String nameA = firstPart;
-        System.out.println("Name A: " + nameA);
+        //System.out.println("Name A: " + nameA);
         //Score A
         String scoreA = score.substring(0, score.indexOf("-"));
-        System.out.println("Score A: " + scoreA);
+        //System.out.println("Score A: " + scoreA);
 
         //Score B
         String scoreB = score.substring(score.indexOf("-") + 1);
-        System.out.println("Score B: " + scoreB);
+        //System.out.println("Score B: " + scoreB);
 
         //Name B
         String nameB = secondPart;
-        System.out.println("Name B: " + nameB);
+        //System.out.println("Name B: " + nameB);
 
 
         Game football = new Game.GameBuilder().nameA(new Name("A", nameA)).nameB(new Name("B", nameB)).scoreA(new Score("A", scoreA)).scoreB(new Score("B", scoreB)).build();
@@ -117,24 +117,23 @@ public class DetectPatterns {
 
         //Name A
         String nameA = firstPart;
-        System.out.println("Name A: " + nameA);
+        //System.out.println("Name A: " + nameA);
 
         //Name B
         String nameB = secondPart.substring(0, secondPart.indexOf(score.substring(0, 1)));
-        System.out.println("Name B: " + nameB);
-        System.out.println("NAME B:" + nameB);
+        //System.out.println("Name B: " + nameB);
 
         //Score A
         String scoreA = score.substring(0, score.indexOf("-"));
-        System.out.println("Score A: " + scoreA);
+        //System.out.println("Score A: " + scoreA);
 
         //Score B
         String scoreB = score.substring(score.indexOf("-") + 1);
-        System.out.println("Score B: " + scoreB);
+        //System.out.println("Score B: " + scoreB);
 
         //Quarter
         String quarter = list.get(list.toArray().length - 2);
-        System.out.println("Quarter: " + quarter);
+        //System.out.println("Quarter: " + quarter);
 
 
         Game americanFootball = new Game.GameBuilder().nameA(new Name("A", nameA)).nameB(new Name("B", nameB)).scoreA(new Score("A", scoreA)).scoreB(new Score("B", scoreB)).period(new Period(quarter)).build();
@@ -173,48 +172,48 @@ public class DetectPatterns {
         int firstIndexScore = firstPart.indexOf("(");
         String nameTennisA = firstPart.substring(0, firstIndexScore);
         nameTennisA = nameTennisA.replace("*", "");
-        System.out.println("Name Player A: " + nameTennisA);
+        //System.out.println("Name Player A: " + nameTennisA);
 
         //Name Player B
         int secondIndexScore = secondPart.indexOf(")");
         String nameTennisB = secondPart.substring(secondIndexScore + 1, secondPart.length());
         nameTennisB = nameTennisB.replace("*", "");
-        System.out.println("Name Player B: " + nameTennisB);
+        //System.out.println("Name Player B: " + nameTennisB);
 
         //Score A
         String scoreA = score.substring(0, score.indexOf("-"));
-        System.out.println("Score A: " + scoreA);
+        //System.out.println("Score A: " + scoreA);
 
         //Score B
         String scoreB = score.substring(score.indexOf("-") + 1);
-        System.out.println("Score B: " + scoreB);
+        //System.out.println("Score B: " + scoreB);
 
         //Games A
         String gamesA = firstPart.substring(firstPart.indexOf(")") + 1);
-        System.out.println("Games A: " + gamesA);
+        //System.out.println("Games A: " + gamesA);
 
         //Games B
         String gamesB = secondPart.substring(0, secondPart.indexOf("("));
-        System.out.println("Games B: " + gamesB);
+        //System.out.println("Games B: " + gamesB);
 
         //SERVING
         boolean isServingA = false;
         boolean isServingB = false;
         if (firstPart.contains("*")) {
-            System.out.println("Serving A: " + "true");
+            //System.out.println("Serving A: " + "true");
             isServingA = true;
         } else if (secondPart.contains("*")) {
-            System.out.println("Serving B: " + "true");
+            //System.out.println("Serving B: " + "true");
             isServingB = true;
         }
 
         //SETS A
         String setsA = firstPart.substring(firstPart.indexOf("(") + 1, firstPart.indexOf(")"));
-        System.out.println("Sets A: " + setsA);
+        //System.out.println("Sets A: " + setsA);
 
         //SETS B
         String setsB = secondPart.substring(secondPart.indexOf("(") + 1, secondPart.indexOf(")"));
-        System.out.println("Sets B: " + setsB);
+        //System.out.println("Sets B: " + setsB);
 
         Game tennis = new Game.GameBuilder().nameA(new Name("A", nameTennisA)).nameB(new Name("B", nameTennisB)).scoreA(new Score("A", scoreA, gamesA, setsA, isServingA)).scoreB(new Score("B", scoreB, gamesB, setsB, isServingB)).build();
 
